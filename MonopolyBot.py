9150cda834adb58c4d750bcecb23640160f72cbb
Anderson Sutandinata <ander.sutandi@gmail.com>
2020-09-26 05:47:11 -0400

    Update MonopolyBot.py

--- MonopolyBot.py
+++ MonopolyBot.py
@@ -9,10 +9,11 @@ import random
 class Player(commands.Cog):
     
     id=0
-    balance = 0
+    balance = 100
     rent = []
     properties = []
     actionCard = []
+    won=False
 
 
     def __init__(self, bot):
@@ -117,17 +118,22 @@ async def on_reaction_add(reaction, user):
         user=bot.get_user(id1)
         await user.send('you have joined the game player 1')
         player1.id=id1
-        beginningCardsP1()
+        player1.actionCard=[16, 21, 25]
+        player1.rent=[11]
+        player1.properties=[2]
     elif reaction.emoji=='\u2611':
         id2=user.id
         user=bot.get_user(id2)
         await user.send('you have joined the game player 2')
         player2.id=id2
-        beginningCardsP2()
+        player2.actionCard=[17, 23]
+        player2.rent=[12, 14]
+        player2.properties=[5]
 
 def beginningCardsP1():
     for i in range(5):
         drawCard(player1)
+
 def beginningCardsP2():
     for i in range(5):
         drawCard(player2)  
@@ -141,10 +147,11 @@ async def identify(ctx):
 @bot.command()
 async def draw(ctx):
     if(ctx.author.id==player1.id):
-         player= player1
+        player= player1
+        drawCard(player1)
     elif(ctx.author.id==player2.id):
-         player= player2
-    drawCard(player2)
+        player= player2
+        drawCard(player2)
     await ctx.author.send("you've just drawn a card! this is your deck now:")
     if(ctx.author.id==player1.id):
         hand=player1.properties+player1.rent+player1.actionCard
@@ -152,9 +159,9 @@ async def draw(ctx):
         hand=player2.properties+player2.rent+player2.actionCard
     for i in range(len(hand)):
         imageVal=hand[i]
-        await ctx.author.send(file=discord.File(f'{imageVal}.png'))  
-    await ctx.author.send('end of your cards')  
-  
+        await ctx.author.send(file=discord.File(f'{imageVal}.png'))
+    await ctx.author.send('end of your cards') 
+
 @bot.command()
 async def printCards(ctx):
     if(ctx.author.id==player1.id):
@@ -166,7 +173,8 @@ async def printCards(ctx):
         await ctx.author.send(file=discord.File(f'{imageVal}.png'))  
     await ctx.author.send('end of your cards')  
 
-@bot.command()
+
+@bot.command()    
 async def printTable(ctx):
     await ctx.author.send("player 1's properties")  
     for i in range(len(player1.properties)):
@@ -177,7 +185,6 @@ async def printTable(ctx):
         imageVal = player2.properties[i]
         await ctx.author.send(file=discord.File(f'{imageVal}.png'))
 
-
 @bot.command()
 async def play(ctx, id):
     id=int(id)
@@ -187,9 +194,11 @@ async def play(ctx, id):
     elif(ctx.author.id==player2.id):
         player=player2
         opponent=player1
+    if(id<11):
+        await ctx.author.send('not a valid play, you can only play rent or action cards')
     if(11<=id and id<=15):#charge other player rent
         if(len(player.rent)>0):
-            if(checkFullSet(player)):
+            if(checkFullSet(ctx, player)):
                 transferMoney(player,opponent, 200)
                 #remove rent card
             else:
@@ -197,29 +206,35 @@ async def play(ctx, id):
         else:
             await ctx.author.send("you don't have a rent card")
     elif(16<=id and id<=20):#house card
-        counted=0
-        for i in range(len(player.actionCard)):
-            if(player.actionCard[i]==16 or player.actionCard[i]==17 or player.actionCard[i]==18 or player.actionCard[i]==19 or player.actionCard[i]==20):
-                counted+=1
-        if(counted>0):
-            if(checkFullSet(player)):
+        if(containsRent(player)):
+            if(checkFullSet(ctx, player)):
                 player1.properties+=1
                 #remove house card
             else:
                 await ctx.author.send("you don't have a full set")
         else:await ctx.author.send("you don't have a house card")
             
-    elif(21<=id and id<=22):#it's my birthday card
-        transferMoney(ctx, player ,otherPlayerpponent, 200)
+    elif(id==21 or id==22):#it's my birthday card
+        if(containsBirthday(player)):
+            transferMoney(ctx, player ,otherPlayerpponent, 200)
+            await ctx.author.send("you have successfully played your birthday card")
+            #remove birthday card
+        else:await ctx.author.send("you don't have a birthday card")
 
-    if(23<=id and id<=24):#double rent play
-        if(checkFullSet(player)and check(len.player.rent>0)):
-            transferMoney(ctx,player,opponent, 400)
+    if(id==23 or id==24):#double rent play
+        if(containsDoubleRent):
+            if(checkFullSet(ctx, player)and check(len.player.rent>0)):
+                transferMoney(ctx,player,opponent, 400)
             #remove rent card, remove double rent card
-    if(25<=id and id<=26):#pass go, draw 2 cards
-        for i in range(2):
-            drawCard(player)
+        else:await ctx.author.send("you don't have a double rent card")
+        
+    if(id==25 or id==26):#pass go, draw 2 cards
+        if(containsPassGo):
+            for i in range(2):
+                drawCard(player)
             await ctx.author.send("you have succesfully played your pass go card")
+            #remove pa
+        else:await ctx.author.send("you don't have a pass go card")
     
 @bot.command()
 async def dm(ctx, message):
@@ -235,6 +250,37 @@ async def on_ready(): # self must be first parameter in every function in class
 async def isWinner(ctx, Player):
     await ctx.send(f'{Player} has won!')
 
+def containsRent(player):
+    counted=0
+    for i in range(len(player.actionCard)):
+        if(player.actionCard[i]==16 or player.actionCard[i]==17 or player.actionCard[i]==18 or player.actionCard[i]==19 or player.actionCard[i]==20):
+            counted+=1
+    if(counted>0):return True
+    else:return False
+
+def containsBirthday(player):
+    counted=0
+    for i in range(len(player.actionCard)):
+        if(player.actionCard[i]==21 or player.actionCard[i]==22):
+            counted+=1
+    if(counted>0):return True
+    else:return False
+    
+def containsDoubleRent(player):
+    counted=0
+    for i in range(len(player.actionCard)):
+        if(player.actionCard[i]==23 or player.actionCard[i]==24):
+            counted+=1
+    if(counted>0):return True
+    else:return False
+
+def containsPassGo(player):
+    counted=0
+    for i in range(len(player.actionCard)):
+        if(player.actionCard[i]==25 or player.actionCard[i]==26):
+            counted+=1
+    if(counted>0):return True
+    else:return False
 def sortArrays():    
     player1.properties.sort()
     player1.rent.sort()
@@ -259,7 +305,10 @@ def checkFullSet(ctx, player):
         if(player.properties[i] == 9 and player.properties[i+1] == 10):
             return True
     return False
-
+def transferMoney(player, opponent, amount):
+    player.balance+=amount
+    opponent.balance-=amount
+    if(opponent.balance<0):player.won=True
 @commands.command()
 async def on_message(message):
     print(message.content)
@@ -279,18 +328,21 @@ async def makeTurn(ctx, Player):
 
 def drawCard(Player):
     i = random.randint(0, len(deck)-1)
+    if(player1 == Player):
+        player = player1
+    else:
+        player = player2
     print(i)
     print(deck[i])
-    print(deck[0])
     if(0 <= i and i < len(deck)):
         print("valid index")
-    if(deck[i] <= 10):
-        Player.properties.append(deck[i])
-    elif(deck[i] <= 15):
-        Player.rent.append(deck[i])
-    else:
-        Player.actionCard.append(deck[i])
-    deck.pop(i);       
+    if(  0 <= deck[i] and deck[i] <= 10):
+        player.properties.append(deck[i])
+    elif(10 < deck[i] and deck[i] <= 15):
+        player.rent.append(deck[i])
+    elif(15 < deck[i] and deck[i] <= 26):
+        player.actionCard.append(deck[i])
+    deck.pop(i);          
 
 def removeActionCard(id, player):
     #player.properties.remove(id)    
