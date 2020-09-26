7bef8715331e2f527c0dee13fa839256e827c41b
andrewwchong <andrewwchong@gmail.com>
2020-09-26 10:37:16 +0800

    Cogs in main

--- cogs/mainCog.py
+++ cogs/mainCog.py
@@ -8,8 +8,6 @@ class MainCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #@commands.command(pass_context=True)
-
     @commands.Cog.listener()
     async def on_ready(self): # self must be first parameter in every function in class
         print("gray squirrelssssss")
@@ -18,8 +16,6 @@ class MainCog(commands.Cog):
     async def ping2(self,ctx):
         await ctx.send("POG")
 
-    async def on_message(self, message):
-        print(message.content)
 
 def setup(bot):
     bot.add_cog(MainCog(bot))
--- hackathonBot.py
+++ hackathonBot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import string
+
+
 bot=commands.Bot(command_prefix='/')
 
 @bot.event
@@ -18,17 +20,28 @@ async def on_ready():
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
+
+#Cog stuff
+
 @bot.command()
-async def load(ctx, extension):
+async def load(ctx, extension): #extension is cog
     bot.load_extension(f'cogs.{extension}')
+    await ctx.send("loaded cog")
+
 @bot.command()
-async def unload(ctx, extension):
+async def unload(ctx, extension): #extension is cog
     bot.unload_extension(f'cogs.{extension}')
+    await ctx.send("unloaded cog")
+
+@bot.command()
+async def reload(ctx, extension): #extension is cog
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send("reloaded cog")
 
 #search cog folder for .py files
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
         bot.load_extension(f'cogs.{filename[:-3]}')
-
 
 bot.run('NzU5MTc0MzUxMzQxMjIzOTU2.X25qNg.-_PsN-qFJaiqt0VaPPg6brDhA2s')
